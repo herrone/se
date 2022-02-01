75618ac49d8ec956182c0cdf68ce64de460e14dd
herrone <egretfulherron@gmail.com>
2022-02-01 18:15:52 +0000

    added to allow containers to build with mongodb

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -26,5 +26,7 @@ public class App
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+        // Connect to MongoDB
+
     }
 }
